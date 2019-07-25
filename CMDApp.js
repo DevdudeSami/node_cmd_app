d53c7842b7428411297703176940342a9662f843
Youssef Moawad <youssef_programming@icloud.com>
2019-07-25 08:50:32 +0100

    Added maximum arguments, exact arguments, ignoring empty commands and stop function.

--- CMDApp.js
+++ CMDApp.js
@@ -3,9 +3,13 @@ const rl = require('readline-sync')
 class CMDApp {
 	commands = {}
 	minimumArgs = {}
+	maximumArgs = {}
+	argsCount = {}
 	quitCommands = ["q", "quit"]
 	help = {}
 	prompt = ">> "
+
+	stopping = false
 
 	setCommands(commands) {
 		this.commands = commands
@@ -13,6 +17,14 @@ class CMDApp {
 
 	setMinimumArgs(minimumArgs) {
 		this.minimumArgs = minimumArgs
+	}
+
+	setMaximumArgs(maximumArgs) {
+		this.maximumArgs = maximumArgs
+	}
+
+	setArgsCount(argsCount) {
+		this.argsCount = argsCount
 	}
 
 	setQuitCommands(quitCommands) {
@@ -28,16 +40,25 @@ class CMDApp {
 	}
 
 	start() {
-		while(true) {
+		this.stopping = false
+		while(!this.stopping) {
 			let command = rl.question(this.prompt).split(' ')
+			// Check whitespace
+			if(command[0].trim() == "") continue
+
 			if(command[0] in this.commands) {
-				if(command[0] in this.minimumArgs && this.minimumArgs[command[0]] >= command.length) {
-					console.log(`The command ${command[0]} expects at least ${this.minimumArgs[command[0]]} arguments.`)
-				}
+
+				// Check minimum args
+				if(command[0] in this.minimumArgs && this.minimumArgs[command[0]] >= command.length) console.log(`The command ${command[0]} expects at least ${this.minimumArgs[command[0]]} arguments.`)
+				// Check maximum args
+				else if(command[0] in this.maximumArgs && this.maximumArgs[command[0]] < command.length-1) console.log(`The command ${command[0]} takes at most ${this.maximumArgs[command[0]]} arguments.`)
+				// Check args count
+				else if(command[0] in this.argsCount && this.argsCount[command[0]] != command.length-1) console.log(`The command ${command[0]} expects exactly ${this.argsCount[command[0]]} arguments.`)
+				// Run command
 				else this.commands[command[0]](command.slice(1))
 			}
 			else if(this.quitCommands.includes(command[0])) {
-				break
+				this.stopping = true
 			}
 			else if(command[0] == "help") {
 				this.printHelp(command)
@@ -49,6 +70,10 @@ class CMDApp {
 				console.log("Unrecognised command.")
 			}
 		}
+	}
+
+	stop() {
+		this.stopping = true
 	}
 
 	printHelp(command) {
--- tester.js
+++ tester.js
@@ -1,14 +1,28 @@
 const CMDApp = require('./CMDApp')
 
+const app = new CMDApp()
+
 const commands = {
 	t: tF,
 	c: cF,
-	g: gF
+	g: gF,
+	y: yF,
+	p: pF,
+	testStop
 }
 
 const minArgs = {
 	t: 3,
 	c: 2,
+}
+
+const maxArgs = {
+	g: 4
+}
+
+const argsCount = {
+	y: 3,
+	p: 2
 }
 
 const help = {
@@ -28,11 +42,26 @@ function cF(args) {
 function gF(args) {
 	console.log("g" + args)
 }
+function pF(args) {
+	console.log("p" + args)
+}
+function yF(args) {
+	console.log("y" + args)
+}
+
+function testStop(args) {
+	console.log("stopping")
+	app.stop()
+
+	console.log("starting")
+	app.start()
+}
 
 
-const app = new CMDApp()
 app.setCommands(commands)
 app.setMinimumArgs(minArgs)
+app.setMaximumArgs(maxArgs)
+app.setArgsCount(argsCount)
 app.setHelp(help)
 app.setPrompt("> ")
 app.start()
